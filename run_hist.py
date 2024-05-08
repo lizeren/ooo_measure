0e05c21b933d4a776c972ad81b15d6a49ee163d9
lizeren <li3103@purdue.edu>
2024-05-08 04:13:37 -0400

    add first quad

--- run_hist.py
+++ run_hist.py
@@ -1,3 +1,53 @@
+# import subprocess
+# import pandas as pd
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+
+# # Function to run the program and capture output
+# def run_program():
+#     # Adjust the command if your executable requires different handling
+#     result = subprocess.run(['./execute1'], capture_output=True, text=True)
+#     return result.stdout
+
+# # Main function to collect data
+# def collect_data(runs=1000):
+#     data = {'Hit': [], 'Miss': [], 'In': [], 'Out': []}
+#     for _ in range(runs):
+#         output = run_program()
+#         # Example output parsing: "hit:91 miss:553 in:108 out:90"
+#         hit_val = int(output.split('hit:')[1].split()[0])
+#         miss_val = int(output.split('miss:')[1].split()[0])
+#         in_val = int(output.split('in:')[1].split()[0])
+#         out_val = int(output.split('out:')[1].split()[0])
+#         data['Hit'].append(hit_val)
+#         data['Miss'].append(miss_val)
+#         data['In'].append(in_val)
+#         data['Out'].append(out_val)
+#     return pd.DataFrame(data)
+
+# # Generate and plot data
+# data = collect_data()
+
+# # Creating multiple plots with a smaller figure size
+# fig, axs = plt.subplots(2, 2, figsize=(10, 8))  # Smaller figure size
+
+# # Plotting histograms for each variable
+# sns.histplot(data['Hit'], kde=False, bins=50, ax=axs[0, 0])
+# axs[0, 0].set_title('Histogram of "Hit" Values')
+
+# sns.histplot(data['Miss'], kde=False, bins=50, ax=axs[0, 1])
+# axs[0, 1].set_title('Histogram of "Miss" Values')
+
+# sns.histplot(data['In'], kde=False, bins=50, ax=axs[1, 0])
+# axs[1, 0].set_title('Histogram of "In" Values')
+
+# sns.histplot(data['Out'], kde=False, bins=50, ax=axs[1, 1])
+# axs[1, 1].set_title('Histogram of "Out" Values')
+
+# plt.tight_layout()
+# plt.show()
+
+
 import subprocess
 import pandas as pd
 import seaborn as sns
@@ -31,18 +81,26 @@ data = collect_data()
 # Creating multiple plots with a smaller figure size
 fig, axs = plt.subplots(2, 2, figsize=(10, 8))  # Smaller figure size
 
-# Plotting histograms for each variable
-sns.histplot(data['Hit'], kde=False, bins=30, ax=axs[0, 0])
-axs[0, 0].set_title('Histogram of "Hit" Values')
+# Plotting histograms for each variable and annotating
+for i, (ax, column) in enumerate(zip(axs.flatten(), data.columns)):
+    sns.histplot(data[column], kde=False, bins=50, ax=ax)
+    ax.set_title(f'')
 
-sns.histplot(data['Miss'], kde=False, bins=30, ax=axs[0, 1])
-axs[0, 1].set_title('Histogram of "Miss" Values')
+    # Find the bin with the highest count
+    max_height = 0
+    max_bin_center = 0
+    for patch in ax.patches:
+        if patch.get_height() > max_height:
+            max_height = patch.get_height()
+            max_bin_center = patch.get_x() + patch.get_width() / 2
 
-sns.histplot(data['In'], kde=False, bins=30, ax=axs[1, 0])
-axs[1, 0].set_title('Histogram of "In" Values')
-
-sns.histplot(data['Out'], kde=False, bins=30, ax=axs[1, 1])
-axs[1, 1].set_title('Histogram of "Out" Values')
+    # Annotate the highest bin with count and x-axis value
+    annotation_text = f'Max count: {int(max_height)} at {max_bin_center:.2f}'
+    ax.annotate(annotation_text, 
+                xy=(max_bin_center, max_height), 
+                xytext=(0, 10),  # 10 points vertical offset
+                textcoords='offset points',
+                ha='center', va='bottom')
 
 plt.tight_layout()
 plt.show()
